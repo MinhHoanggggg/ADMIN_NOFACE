7a4c166ddb2188fd79ca3fe0aae038600117a9f8
minhhoanggggg <iker23062000@gmail.com>
2021-12-09 22:15:44 +0700

    hehe

---
+++ NOFACE_ADMIN/Controllers/AvatarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NOFACE_ADMIN.Controllers
+{
+    public class AvatarController : Controller
+    {
+        // GET: Avatar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
